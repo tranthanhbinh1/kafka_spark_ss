ce25c655a94a60a75a9a973c1e585ec759d8d331
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-05-28 21:34:04 +0700

    minor changes

--- spark_streaming.py
+++ spark_streaming.py
@@ -27,25 +27,3 @@ query = df.writeStream.outputMode("append").format("console").start()
 # Wait for the termination of the streaming query
 query.awaitTermination()
 
-
-# # Apply the schema and convert the value column from Kafka to JSON format
-# df = df.withColumn("value", col("value").cast("string"))
-# df = df.withColumn("jsonData", from_json("value", schema))
-
-# # # Extract the relevant fields from the JSON data
-# # df = df.select("jsonData.*")
-
-# # Transformation
-# df = df.selectExpr("s", "explode(c) as c", "explode(h) as h", "explode(l) as l", "explode(o) as o", "explode(t) as t", "explode(v) as v")
-
-# # Write the processed data to the desired sink
-
-# # query = df.writeStream.format("json") \
-# #     .option("path", "stock_data.json") \
-# #     .option("checkpointLocation", "checkpoint") \
-# #     .start()
-
-# query = df.writeStream.format("console").start()
-
-# # Start the streaming query
-# query.start().awaitTermination()
