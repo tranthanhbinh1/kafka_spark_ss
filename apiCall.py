dcbbb0614906707d62a8df9fb34d4e110ca751b1
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-05-28 15:21:36 +0700

    minor changes

--- apiCall.py
+++ apiCall.py
@@ -1,12 +1,10 @@
 import finnhub, time, json
 from kafka import KafkaProducer
 
-
-
 # Setup finnhub api
 api_key = "chbqth1r01quf00vuei0chbqth1r01quf00vueig"
 finnhub_client = finnhub.Client(api_key=api_key)
-symbols = ['AAPL', 'AMZN', 'GOOG', 'MSFT', 'TSLA', 'NVDA', 'NFLX']
+symbols = ['AAPL', 'AMZN', 'GOOG', 'MSFT', 'NFLX']
 
 # Calculate the start and end timestamps for a 1-minute interval
 end_timestamp = int(time.time())
@@ -18,20 +16,21 @@ end_timestamp = int(end_timestamp)
 
 # Create a Kafka Producer instance
 bootstrap_servers = "localhost:29092"
-topic = "stock_analyzer"
+topic1 = "stock_analyzer"
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
 # Publish data to Kafka
 def publish_to_kafka(topic, data):
-    producer.send(topic, data.encode('utf-8'))
+    encoded_data = json.dumps(data).encode('utf=8')
+    producer.send(topic, encoded_data)
     producer.flush()
 
 # Call the API function and publish data to Kafka
 while True:
     for symbol in symbols:
         res = finnhub_client.stock_candles(symbol, 'D', start_timestamp, end_timestamp)
-        data = json.dumps(res)
-        publish_to_kafka(topic, data)
+        print(res)
+        publish_to_kafka(topic1, res)
     time.sleep(10)
 
 
--- spark_streaming.py
+++ spark_streaming.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import explode
 from pyspark.sql.functions import from_json, col
 
 # Define the Kafka broker and topic
-bootstrap_servers = "localhost:29092"
+bootstrap_server = "localhost:29092"
 topic = "stock_analyzer"
 
 spark = SparkSession.builder.appName("StockDashboard").getOrCreate()
@@ -11,29 +11,46 @@ spark = SparkSession.builder.appName("StockDashboard").getOrCreate()
 # Define the schema for the stock data
 schema = "value string"
 
-# Read the streaming data from Kafka
-df = spark.readStream.format("kafka")\
-    .option("kafka.bootstrap.servers", bootstrap_servers)\
-    .option("subscribe", topic).load()
+# # Read the streaming data from Kafka
+# df = spark.readStream.format("kafka")\
+#     .option("kafka.bootstrap.servers", bootstrap_server)\
+#     .option("subscribe", topic).load()
 
-# Apply the schema and convert the value column from Kafka to JSON format
-df = df.withColumn("value", col("value").cast("string"))
-df = df.withColumn("jsonData", from_json("value", schema))
+df = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", bootstrap_server) \
+  .option("subscribe", topic) \
+  .load()
 
-# Extract the relevant fields from the JSON data
-df = df.select("jsonData.*")
+# Convert the value column from binary to string
+df = df.withColumn("value", df["value"].cast("string"))
 
-# Transformation
-df = df.selectExpr("s", "explode(c) as c", "explode(h) as h", "explode(l) as l", "explode(o) as o", "explode(t) as t", "explode(v) as v")
+# Print the data to the console
+query = df.writeStream.outputMode("append").format("console").start()
 
-# Write the processed data to the desired sink
+# Wait for the termination of the streaming query
+query.awaitTermination()
 
-# query = df.writeStream.format("json") \
-#     .option("path", "stock_data.json") \
-#     .option("checkpointLocation", "checkpoint") \
-#     .start()
 
-query = df.writeStream.format("console").start()
+# # Apply the schema and convert the value column from Kafka to JSON format
+# df = df.withColumn("value", col("value").cast("string"))
+# df = df.withColumn("jsonData", from_json("value", schema))
 
-# Start the streaming query
-query.start().awaitTermination()
+# # # Extract the relevant fields from the JSON data
+# # df = df.select("jsonData.*")
+
+# # Transformation
+# df = df.selectExpr("s", "explode(c) as c", "explode(h) as h", "explode(l) as l", "explode(o) as o", "explode(t) as t", "explode(v) as v")
+
+# # Write the processed data to the desired sink
+
+# # query = df.writeStream.format("json") \
+# #     .option("path", "stock_data.json") \
+# #     .option("checkpointLocation", "checkpoint") \
+# #     .start()
+
+# query = df.writeStream.format("console").start()
+
+# # Start the streaming query
+# query.start().awaitTermination()
